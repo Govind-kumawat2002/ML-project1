a04216411fbbd95512cfbea5d13ad292d4204e13
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-01-25 11:33:33 +0530

    mongodb and mysql connection is done

---
+++ source_code/dbconfig.py
@@ -0,0 +1,122 @@
+import mysql.connector
+from mysql.connector import Error
+from source_code.logger import logging
+from source_code.exception import InsuranceException
+from dotenv import load_dotenv
+import os,sys
+import pandas as pd
+from pymongo import MongoClient
+load_dotenv()
+mysql_user = os.getenv('mysql_user')
+mysql_password = os.getenv('mysql_user_pass')
+mysql_database= os.getenv('mysql_database')
+# print(mysql_user,mysql_password,mysql_database)
+mogodb_string=os.getenv("mogodb_string")
+mogo_db_name=os.getenv('mogo_db_name')
+mogo_collection_name=os.getenv('mogo_collection_name')
+
+def connect_to_mogodb():
+    try:
+        client =MongoClient(mogodb_string)
+        print("succesfully cooneted to mongodb")
+        # mogo_database=client[mogo_db_name]
+        # mogo_collection=mogo_database[mogo_collection_name]
+        return client
+    except Exception as e:
+        custom_exception=InsuranceException(e,sys)
+        print(f"unable to connect to the mogodb{ custom_exception.error_message}")
+        logging.error(custom_exception.error_message)
+        # print(f"Error connecting to MySQL: {e}")
+        raise InsuranceException(e,sys)
+        return None
+
+
+def connect_to_mysql():
+    try:
+        # Connect to the MySQL server
+        connection = mysql.connector.connect(
+            host='localhost',       # Replace with your host, e.g., '127.0.0.1'
+            user='root',            # Replace with your MySQL username
+            password='1234',        # Replace with your MySQL password
+            database='mlproject_db', # Replace with your database name
+            allow_local_infile=True  # Enable loading local files
+        )
+        logging.info("connecting to with database..")
+        
+        if connection.is_connected():
+            print("Connected to MySQL Server")
+            logging.info("successfully connected with database..")
+            return connection
+    except Error as e:
+        custom_exception=InsuranceException(e,sys)
+        custom_exception.error_message
+        logging.error(custom_exception.error_message)
+        # print(f"Error connecting to MySQL: {e}")
+        raise InsuranceException(e,sys)
+        return None
+    
+
+
+
+
+
+
+
+# def create_table(connection):
+#     try:
+#         cursor = connection.cursor()
+#         # Define the table structure
+#         create_table_query = """
+#         CREATE TABLE IF NOT EXISTS insurance_data (
+#             bmi FLOAT NOT NULL,
+#             children INT NOT NULL,
+#             smoker VARCHAR(10) NOT NULL,
+#             region VARCHAR(50) NOT NULL,
+#             charges FLOAT NOT NULL
+#         );
+#         """
+#         cursor.execute(create_table_query)
+#         print("Table 'insurance_data' created successfully (or already exists).")
+#     except Error as e:
+#         custom_exception=InsuranceException(e,sys)
+#         custom_exception.error_message
+#         logging.error(custom_exception.error_message)
+#         # print(f"Error creating table: {e}")
+#         raise InsuranceException(e,sys)
+
+# def dump_data_sql(connection, csv_file_path):
+#     try:
+#         cursor = connection.cursor()
+#         # Use the LOCAL option to load from a local file
+#         load_data_query = f"""
+#         LOAD DATA LOCAL INFILE '{csv_file_path}'
+#         INTO TABLE insurance_data
+#         FIELDS TERMINATED BY ','   -- Column separator
+#         ENCLOSED BY '"'            -- Text delimiter (optional)
+#         LINES TERMINATED BY '\\n'  -- Row delimiter
+#         IGNORE 1 LINES;           -- Ignore header row
+#         """
+#         cursor.execute(load_data_query)
+#         connection.commit()  # Commit the changes to the database
+#         print("Data loaded successfully from CSV.")
+#     except Error as e:
+#         # print(f"Error loading data from CSV: {e}")
+#         custom_exception=InsuranceException(e,sys)
+#         custom_exception.error_message
+#         logging.error(custom_exception.error_message)
+
+# # Connect to MySQL
+# connection = connect_to_mysql()
+# if connection:
+#     # Create the table if it doesn't exist
+#     create_table(connection)
+    
+#     # Specify the path to your CSV file
+#     csv_file_path = './firsthalf_data.csv'  # Update this path
+    
+#     # Load data from CSV into the table
+#     dump_data_sql(connection, csv_file_path)
+
+#     # Close connection
+#     connection.close()
+#     print("Connection closed.")
